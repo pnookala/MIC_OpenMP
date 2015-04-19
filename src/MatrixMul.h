bc84c5eb2df7bb6e396c83d4891e474a9d2ab7e7
pnookala <pnookala@users.noreply.github.com>
2015-04-19 18:30:27 -0500

    Update MatrixMul.h

--- src/MatrixMul.h
+++ src/MatrixMul.h
@@ -15,13 +15,20 @@
 
 #define basetype int
 #define basetypeprint "%d"
-
+/* This code is for offload execution */
 __attribute__ ((target(mic))) typedef struct matrix2d {
 	basetype** data;
 	int rows;
 	int cols;
 } matrix2d;
+/* This code is for native execution */
+/*typedef struct matrix2d {
+	basetype** data;
+	int rows;
+	int cols;
+} matrix2d;*/
 
+/* This code is for native execution */
 /*matrix2d* loadMatrixFile(char* file);
 matrix2d* createMatrix(int rows, int cols);
 void randomizeMatrix(matrix2d* mat);
@@ -29,6 +36,8 @@ matrix2d* multiplyMatrices(matrix2d* A, matrix2d* B);
 void printMatrix(matrix2d* mat, char format);
 void deleteMatrix(matrix2d* mat);
 */
+
+/* This code is for offload execution */
 __attribute__ ((target(mic))) matrix2d* loadMatrixFile(char* file);
 __attribute__ ((target(mic))) matrix2d* createMatrix(int rows, int cols);
 __attribute__ ((target(mic))) void randomizeMatrix(matrix2d* mat);
